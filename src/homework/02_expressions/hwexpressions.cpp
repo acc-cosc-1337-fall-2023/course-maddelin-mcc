3211716c1750827c81154d0809c0320ba59baf9f
maddelin-mcc <143145154+maddelin-mcc@users.noreply.github.com>
2023-09-09 22:38:08 +0000

    Submitting hw2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -7,7 +7,16 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+double get_sales_tax_amount(double meal_amount)
+{
+	double tax_amount = meal_amount * tax_rate;
+	return tax_amount;
+}
 
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	double tip_amount = meal_amount * tip_rate/100;
+	return tip_amount;
+}
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,33 @@
-﻿//write include statements
+﻿#include <iostream>
+#include "hwexpressions.h"
 
-
-//write namespace using statement for cout
+using std::cout;
+using std::cin;
 
 int main()
 {
-	
+    double meal_amount;
+    double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
 
-	return 0;
+    cout<<"\nRestaurant Receipt\n\nEnter the Meal Amount: $";
+    cin>>meal_amount;
+    tax_amount = get_sales_tax_amount(meal_amount);
+    
+    cout<<"Enter Tip Percentage: ";
+    cin>>tip_rate;
+    tip_amount = get_tip_amount(meal_amount, tip_rate);
+	total = meal_amount+tax_amount+tip_amount;
+
+	cout
+	<<"\n\n**********************\nReceipt\n**********************\n"
+    <<"\nMeal Amount: $"<<meal_amount
+	<<"\nSales Tax:   $"<<tax_amount
+	<<"\nTip:         $"<<tip_amount
+	<<"\nTotal:       $"<<total
+	<<"\n\nThank you!\n*********************\n";
+
+    return 0;
 }
