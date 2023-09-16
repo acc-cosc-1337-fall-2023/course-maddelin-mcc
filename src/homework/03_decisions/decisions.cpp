ef8b85f8d9ed240c1fa61dbe306833bb51bb3f13
maddelin-mcc <143145154+maddelin-mcc@users.noreply.github.com>
2023-09-16 19:30:40 +0000

    Submitting HW 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,59 @@
 //write include statement for decisions header
-
+#include "decisions.h"
+using std::string;
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string result;
+    if(grade >= 90)
+    {
+        result = "A";
+    }
+    else if(grade >= 80)
+    {
+        result = "B";
+    }
+    else if(grade >= 70)
+    {
+        result = "C";
+    }
+    else if(grade >= 60)
+    {
+        result = "D";
+    }
+    else
+    {
+        result = "F";
+    }
+    return result;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string result;
+
+    switch (grade/10)
+    {
+    case 10:
+        result = "A";
+        break;
+    case 9: 
+        result = "A";
+        break;
+    case 8: 
+        result = "B";
+        break;
+    case 7: 
+        result = "C";
+        break;
+    case 6: 
+        result = "D";
+        break;
+    default:
+        result = "F";
+        break;
+    }
+
+    return result;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,25 @@
 //write include statements
+#include <iostream>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	int grade;
+	string letter_grade_if;
+	string letter_grade_switch;
+	
+	cout<<"Enter a number grade: ";
+	cin>>grade;
+
+	letter_grade_if = get_letter_grade_using_if(grade);
+	letter_grade_switch = get_letter_grade_using_switch(grade);
+
+	cout<<"The letter grade using if is: "<<letter_grade_if<<"\n";
+	cout<<"The letter grade using switch is: "<<letter_grade_switch<<"\n";
+
 	return 0;
 }
