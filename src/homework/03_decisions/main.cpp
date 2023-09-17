0f67a556d5416c1309f4b8d45129478445090f06
maddelin-mcc <143145154+maddelin-mcc@users.noreply.github.com>
2023-09-17 01:11:58 +0000

    submitting updated main.cpp for hw3

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,3 +1,13 @@
+/*
+        MAIN MENU
+
+1-Letter grade using if
+2-Letter grade using switch
+3-Exit
+
+In options 1 and 2, prompt the user for a number and if the number is between 0 and 100 call the appropriate function; 
+otherwise, display a message that the number is out of range.*/
+
 //write include statements
 #include <iostream>
 #include "decisions.h"
@@ -9,17 +19,43 @@ using std::string;
 int main() 
 {
 	int grade;
+	int selection;
 	string letter_grade_if;
 	string letter_grade_switch;
-	
-	cout<<"Enter a number grade: ";
-	cin>>grade;
 
-	letter_grade_if = get_letter_grade_using_if(grade);
-	letter_grade_switch = get_letter_grade_using_switch(grade);
+	cout<<"	 MAIN MENU\n1-Letter grade using if\n2-Letter grade using switch\n3-Exit\n\n";
+	cin>>selection;
 
-	cout<<"The letter grade using if is: "<<letter_grade_if<<"\n";
-	cout<<"The letter grade using switch is: "<<letter_grade_switch<<"\n";
-
+	switch (selection)
+	{
+		case 1:
+			cout<<"Enter a number grade: ";
+			cin>>grade;
+			if(grade>100 || grade < 0)
+			{
+				cout<<"Grade value is out of range\n";
+			}
+			else
+			{
+				letter_grade_if = get_letter_grade_using_if(grade);
+				cout<<"The letter grade using if is: "<<letter_grade_if<<"\n";
+			}
+			break;
+		case 2:
+			cout<<"Enter a number grade: ";
+			cin>>grade;
+			if(grade>100 || grade < 0)
+			{
+				cout<<"Grade value is out of range\n";
+			}
+			else
+			{
+				letter_grade_switch = get_letter_grade_using_switch(grade);
+				cout<<"The letter grade using switch is: "<<letter_grade_switch<<"\n";
+			}
+			break;
+		default:
+			break;
+	}
 	return 0;
 }
